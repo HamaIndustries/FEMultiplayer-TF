81d3277bf7efb8989718e7b8d71a4e5b1b023bed
Elijah Mitchell <eliatlarge@gmail.com>
2016-10-11 21:03:18 -0500

    Add LibGDX files

---
+++ desktop/src/net/fe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.fe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.fe.FEMultiplayer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FEMultiplayer(), config);
+	}
+}
